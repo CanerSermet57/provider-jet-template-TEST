a72cfe270d4b3bda2e5f7481fa575fd7bcb2dfbf
Caner Şermet <canersermet2@gmail.com>
2022-05-08 00:08:10 +0300

    Github Test Prepare

--- internal/clients/github.go
+++ internal/clients/github.go
@@ -19,6 +19,7 @@ package clients
 import (
 	"context"
 	"encoding/json"
+	"fmt"
 
 	"github.com/crossplane/crossplane-runtime/pkg/resource"
 	"github.com/pkg/errors"
@@ -37,6 +38,15 @@ const (
 	errTrackUsage           = "cannot track ProviderConfig usage"
 	errExtractCredentials   = "cannot extract credentials"
 	errUnmarshalCredentials = "cannot unmarshal github credentials as JSON"
+)
+
+const (
+	keyBaseURL = "base_url"
+	keyOwner   = "owner"
+	keyToken   = "token"
+
+	// GitHub credentials environment variable names
+	envToken = "GITHUB_TOKEN"
 )
 
 // TerraformSetupBuilder builds Terraform a terraform.SetupFn function which
@@ -87,6 +97,19 @@ func TerraformSetupBuilder(version, providerSource, providerVersion string) terr
 			"username": githubCreds["username"],
 			"password": githubCreds["password"],
 		}*/
+
+		// set provider configuration
+		ps.Configuration = map[string]interface{}{}
+		if v, ok := githubCreds[keyBaseURL]; ok {
+			ps.Configuration[keyBaseURL] = v
+		}
+		if v, ok := githubCreds[keyOwner]; ok {
+			ps.Configuration[keyOwner] = v
+		}
+		// set environment variables for sensitive provider configuration
+		ps.Env = []string{
+			fmt.Sprintf("%s=%s", envToken, githubCreds[keyToken]),
+		}
 		return ps, nil
 	}
 }
